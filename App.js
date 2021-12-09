92d7f17a8a7d5c54309f8ae2f1f4c0d37ef2e10b
t9niro00 <t9niro00@students.oamk.fi>
2021-12-09 22:24:25 +0200

    second commit when the app UI is almost done.

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
+      
+      {/* Today's tasks */}
+
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+
+
+        <View style={styles.item}>
+
+        {/* Tasks go here */}
+        <Task text={'task 1'}/>
+        <Task text={'task 2'}/>
+
+      </View>
+    </View>
     </View>
   );
 }
@@ -14,8 +28,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  tasksWrapper: {
+    paddingTop: 25,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  item: {},
 });
